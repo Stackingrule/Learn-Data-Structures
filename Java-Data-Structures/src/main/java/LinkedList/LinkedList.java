b2d68337910b6bb5d5d4afcac4678c065c3f6af6
Stackingrule <messageswitching@gmail.com>
2019-12-19 20:58:16 +0800

    complete ArrayStack & fixed Array & LinkedList

--- Java-Data-Structures/src/main/java/LinkedList/LinkedList.java
+++ Java-Data-Structures/src/main/java/LinkedList/LinkedList.java
@@ -2,141 +2,171 @@ package LinkedList;
 
 public class LinkedList<E> {
 
-    private class Node {
-        private E element;
-        private Node next;
+    private class Node{
+        public E e;
+        public Node next;
 
-        public Node(E element, Node next) {
-            this.element = element;
+        public Node(E e, Node next){
+            this.e = e;
             this.next = next;
         }
 
-        public Node(E element) {
-            this.element = element;
+        public Node(E e){
+            this(e, null);
         }
 
-        public Node() {
+        public Node(){
             this(null, null);
         }
 
-        public String toString() {
-            return element.toString();
+        @Override
+        public String toString(){
+            return e.toString();
         }
-
     }
 
     private Node dummyHead;
     private int size;
 
-    public LinkedList() {
-        dummyHead = new Node(null);
+    public LinkedList(){
+        dummyHead = new Node();
         size = 0;
     }
 
-    public int getSize() {
+
+    public int getSize(){
         return size;
     }
 
-    public boolean isEmpty() {
+
+    public boolean isEmpty(){
         return size == 0;
     }
 
-    public void add(E element, int index) {
-        if(index < 0 || index > size) {
+
+    public void add(int index, E e){
+
+        if(index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Illegal index.");
-        }
 
-        Node current = dummyHead;
-        for (int i = 0; i < index; i ++) {
-            current = current.next;
-        }
+        Node prev = dummyHead;
+        for(int i = 0 ; i < index ; i ++)
+            prev = prev.next;
 
-        current.next = new Node(element, current.next);
+        prev.next = new Node(e, prev.next);
         size ++;
     }
 
-    public E get(int index) {
-        if (index < 0 || index > size) {
+
+    public void addFirst(E e){
+        add(0, e);
+    }
+
+    public void addLast(E e){
+        add(size, e);
+    }
+
+
+    public E get(int index){
+
+        if(index < 0 || index >= size)
             throw new IllegalArgumentException("Get failed. Illegal index.");
-        }
 
-        Node current = dummyHead;
-        for (int i = 0; i < index; i ++) {
-            current = current.next;
-        }
-
-        return current.element;
+        Node cur = dummyHead.next;
+        for(int i = 0 ; i < index ; i ++)
+            cur = cur.next;
+        return cur.e;
     }
 
-    public void set(int index, E element) {
-        if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Set failed. Illegal index.");
-        }
 
-        Node current = dummyHead;
-        for (int i = 0; i < index; i ++) {
-            current = current.next;
-        }
-
-        current.element = element;
+    public E getFirst(){
+        return get(0);
     }
 
-    private boolean contains(E element) {
-        Node current = dummyHead.next;
 
-        while (current != null) {
-            if (current.element.equals(element))
+    public E getLast(){
+        return get(size - 1);
+    }
+
+
+    public void set(int index, E e){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Update failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for(int i = 0 ; i < index ; i ++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+
+    public boolean contains(E e){
+        Node cur = dummyHead.next;
+        while(cur != null){
+            if(cur.e.equals(e))
                 return true;
-            current = current.next;
+            cur = cur.next;
         }
         return false;
     }
 
-    public E remove(int index) {
-        if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Set failed. Illegal index.");
-        }
+    public E remove(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
 
-        Node current = dummyHead;
-        for (int i = 0; i < index; i ++) {
-            current = current.next;
-        }
 
-        Node retNode = current.next;
-        current.next = retNode.next;
+
+        Node prev = dummyHead;
+        for(int i = 0 ; i < index ; i ++)
+            prev = prev.next;
+
+        Node retNode = prev.next;
+        prev.next = retNode.next;
         retNode.next = null;
         size --;
 
-        return retNode.element;
-
+        return retNode.e;
     }
 
-    public void removeElement(E element) {
-        Node current = dummyHead;
-        while (current.next != null) {
-            if (current.element.equals(element))
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+
+    public void removeElement(E e){
+
+        Node prev = dummyHead;
+        while(prev.next != null){
+            if(prev.next.e.equals(e))
                 break;
-            current = current.next;
+            prev = prev.next;
         }
-        if (current.next != null) {
-            Node retNode = current.next;
-            current.next = retNode.next;
-            retNode.next = null;
+
+        if(prev.next != null){
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
             size --;
         }
     }
 
     @Override
-    public String toString() {
-        StringBuilder  stringBuilder = new StringBuilder();
+    public String toString(){
+        StringBuilder res = new StringBuilder();
 
-        Node current = dummyHead.next;
-        while (current != null) {
-            stringBuilder.append(current + "->");
+        Node cur = dummyHead.next;
+        while(cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
         }
-        stringBuilder.append("NULL");
+        res.append("NULL");
 
-        return stringBuilder.toString();
+        return res.toString();
     }
-
 }
--- Java-Data-Structures/src/main/java/Stack/Array.java
+++ Java-Data-Structures/src/main/java/Stack/Array.java
@@ -1,4 +1,4 @@
-package Arrays;
+package Stack;
 
 
 public class Array<E> {
@@ -6,30 +6,33 @@ public class Array<E> {
     private E[] data;
     private int size;
 
-
+    // 构造函数，传入数组的容量capacity构造Array
     public Array(int capacity){
         data = (E[])new Object[capacity];
         size = 0;
     }
 
-
+    // 无参数的构造函数，默认数组的容量capacity=10
     public Array(){
         this(10);
     }
 
+    // 获取数组的容量
     public int getCapacity(){
         return data.length;
     }
 
+    // 获取数组中的元素个数
     public int getSize(){
         return size;
     }
 
-
+    // 返回数组是否为空
     public boolean isEmpty(){
         return size == 0;
     }
 
+    // 在index索引的位置插入一个新元素e
     public void add(int index, E e){
 
         if(index < 0 || index > size)
@@ -46,16 +49,17 @@ public class Array<E> {
         size ++;
     }
 
-
+    // 向所有元素后添加一个新元素
     public void addLast(E e){
         add(size, e);
     }
 
+    // 在所有元素前添加一个新元素
     public void addFirst(E e){
         add(0, e);
     }
 
-
+    // 获取index索引位置的元素
     public E get(int index){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Get failed. Index is illegal.");
@@ -70,14 +74,14 @@ public class Array<E> {
         return get(0);
     }
 
-
+    // 修改index索引位置的元素为e
     public void set(int index, E e){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Set failed. Index is illegal.");
         data[index] = e;
     }
 
-
+    // 查找数组中是否有元素e
     public boolean contains(E e){
         for(int i = 0 ; i < size ; i ++){
             if(data[i].equals(e))
@@ -86,7 +90,7 @@ public class Array<E> {
         return false;
     }
 
-
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
     public int find(E e){
         for(int i = 0 ; i < size ; i ++){
             if(data[i].equals(e))
@@ -95,7 +99,7 @@ public class Array<E> {
         return -1;
     }
 
-
+    // 从数组中删除index位置的元素, 返回删除的元素
     public E remove(int index){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Remove failed. Index is illegal.");
@@ -111,17 +115,17 @@ public class Array<E> {
         return ret;
     }
 
-
+    // 从数组中删除第一个元素, 返回删除的元素
     public E removeFirst(){
         return remove(0);
     }
 
-
+    // 从数组中删除最后一个元素, 返回删除的元素
     public E removeLast(){
         return remove(size - 1);
     }
 
-
+    // 从数组中删除元素e
     public void removeElement(E e){
         int index = find(e);
         if(index != -1)
@@ -143,7 +147,7 @@ public class Array<E> {
         return res.toString();
     }
 
-
+    // 将数组空间的容量变成newCapacity大小
     private void resize(int newCapacity){
 
         E[] newData = (E[])new Object[newCapacity];
