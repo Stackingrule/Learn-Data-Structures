dfa393bd49738452cab9762785e4163507892672
Stackingrule <messageswitching@gmail.com>
2020-02-12 00:00:24 +0800

    add ArrayList

--- Learn-Data-Structures/Data-Structures-in-Java/src/LinkedList/LinkedList.java
+++ Learn-Data-Structures/Data-Structures-in-Java/src/LinkedList/LinkedList.java
@@ -1,13 +1,20 @@
 package LinkedList;
 
-public class LinkedList {
+public class LinkedList<E> {
 
     private int size;
-    private Node dummyNode; // 头节点
+    private Node<E> dummyNode; // 头节点
 
     private static class Node<E> {
         E element;
         Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
     }
+
+
 
 }
